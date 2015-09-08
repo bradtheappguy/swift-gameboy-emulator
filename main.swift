0b8fa07de8093054f4e2c5bdaea38a572303c2e9
Brad Anderson Smith <brad@theappguy.guru>
2015-09-08 03:03:32 -0700

    add the 16 bit internal registers

--- main.swift
+++ main.swift
@@ -1,7 +1,19 @@
 class CPU {
+  var AF: UInt16 = 0x0
+  var BC: UInt16 = 0x0
+  var DE: UInt16 = 0x0
+  var HL: UInt16 = 0x0
+  var SP: UInt16 = 0x0
+  var PC: UInt16 = 0x0
+
 
   func debugDescription() -> String {
-    return "CPU"
+    return  "AF: \(AF)\n" +
+            "BC: \(BC)\n" +
+            "DE: \(DE)\n" +
+            "HL: \(HL)\n" +
+            "SP: \(SP)\n" +
+            "PC: \(PC)\n"
   }
 }
 
